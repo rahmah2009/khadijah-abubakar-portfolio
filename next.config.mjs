e6d1543f1227db5ecf7d10487e1589d85efaef20
omuhamma <muhammadrahmah005@gmail.com>
2026-07-09 14:05:47 +0100

    Ignore ESLint during production build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
